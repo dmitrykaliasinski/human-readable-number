407ad35e124afe434d55b198ab63ce37955a19c4
dmitrykaliasinski <d.k.akadiman@gmail.com>
2022-01-17 21:51:50 +0300

    fix: trim() for output

--- src/index.js
+++ src/index.js
@@ -1,60 +1,62 @@
 module.exports = function toReadable(number) {
     const one = new Array(
         "",
-        "one",
-        "two",
-        "three",
-        "four",
-        "five",
-        "six",
-        "seven",
-        "eight",
-        "nine",
-        "ten",
-        "eleven",
-        "twelve",
-        "thirteen",
-        "fourteen",
-        "fifteen",
-        "sixteen",
-        "seventeen",
-        "eighteen",
-        "nineteen"
+        " one",
+        " two",
+        " three",
+        " four",
+        " five",
+        " six",
+        " seven",
+        " eight",
+        " nine",
+        " ten",
+        " eleven",
+        " twelve",
+        " thirteen",
+        " fourteen",
+        " fifteen",
+        " sixteen",
+        " seventeen",
+        " eighteen",
+        " nineteen"
     );
     const tens = new Array(
         "",
         "",
-        "twenty",
-        "thirty",
-        "forty",
-        "fifty",
-        "sixty",
-        "seventy",
-        "eighty",
-        "ninety"
+        " twenty",
+        " thirty",
+        " forty",
+        " fifty",
+        " sixty",
+        " seventy",
+        " eighty",
+        " ninety"
     );
-    const hundred = "hundred";
+
     let strNum = number.toString();
     let output = "";
 
+    if (number === 0) {
+        return "zero";
+    }
     if (number < 20) {
         output = one[number];
-        return output;
+        return output.trim();
     }
     if (strNum.length === 2) {
         output = tens[parseInt(strNum.charAt(0))];
-        output = output + " " + one[parseInt(strNum.charAt(1))];
-        return output;
+        output = output + one[parseInt(strNum.charAt(1))];
+        return output.trim();
     }
     if (strNum.length === 3 && strNum.slice(1) >= 10 && strNum.slice(1) < 20) {
-        output = one[parseInt(strNum.charAt(0))] + " " + "hundred";
-        output = output + " " + one[parseInt(strNum.slice(1))];
-        return output;
+        output = one[parseInt(strNum.charAt(0))] + " hundred";
+        output = output + one[parseInt(strNum.slice(1))];
+        return output.trim();
     } else {
-        output = one[parseInt(strNum.charAt(0))] + " " + "hundred";
-        output = output + " " + tens[parseInt(strNum.charAt(1))];
-        output = output + " " + one[parseInt(strNum.charAt(2))];
-        return output;
+        output = one[parseInt(strNum.charAt(0))] + " hundred";
+        output = output + tens[parseInt(strNum.charAt(1))];
+        output = output + one[parseInt(strNum.charAt(2))];
+        return output.trim();
     }
-    return output;
 };
